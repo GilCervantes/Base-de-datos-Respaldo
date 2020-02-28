db8a7149b4da60315022536ed418f441b9516ca5
BryanAguilar24 <60278397+BryanAguilar24@users.noreply.github.com>
2020-02-27 19:07:50 -0700

    Add files via upload

---
+++ enrutame_llaves_foraneas.sql
@@ -0,0 +1,404 @@
+
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: procedimientoalmacenado
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+create database enrutame_triggers;
+use enrutame_triggers;
+--
+-- Table structure for table `calificacion`
+--
+
+DROP TABLE IF EXISTS `calificacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `calificacion` (
+  `id` int(11) DEFAULT NULL,
+  `califacion` int(5) DEFAULT NULL,
+  KEY `integradora` (`califacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `calificacion`
+--
+
+LOCK TABLES `calificacion` WRITE;
+/*!40000 ALTER TABLE `calificacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `calificacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factores`
+--
+
+DROP TABLE IF EXISTS `factores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `factores` (
+  `id` int(11) DEFAULT NULL,
+  `climatologico` varchar(30) DEFAULT NULL,
+  `trafico` varchar(30) DEFAULT NULL,
+  `hora_pico` varchar(30) DEFAULT NULL,
+  KEY `FACTORESM` (`id`,`climatologico`,`trafico`,`hora_pico`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `factores`
+--
+
+LOCK TABLES `factores` WRITE;
+/*!40000 ALTER TABLE `factores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `factores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `clima_factor` BEFORE INSERT ON `factores` FOR EACH ROW begin
+set new.climatologico = 'El clima esta afectando los tiempos.';
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `traf_factor` BEFORE INSERT ON `factores` FOR EACH ROW begin
+set new.trafico = 'Hay trafico, se demorara en llegar el bus.';
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `horapico_factor` BEFORE INSERT ON `factores` FOR EACH ROW begin
+set new.hora_pico = 'Puede que haya mucha gente a bordo del bus.';
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Temporary view structure for view `retraso_limite`
+--
+
+DROP TABLE IF EXISTS `retraso_limite`;
+/*!50001 DROP VIEW IF EXISTS `retraso_limite`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `retraso_limite` AS SELECT 
+ 1 AS `id`,
+ 1 AS `traslado`,
+ 1 AS `retraso`,
+ 1 AS `recorrido_total`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `retraso_lluvia`
+--
+
+DROP TABLE IF EXISTS `retraso_lluvia`;
+/*!50001 DROP VIEW IF EXISTS `retraso_lluvia`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `retraso_lluvia` AS SELECT 
+ 1 AS `id`,
+ 1 AS `climatologico`,
+ 1 AS `trafico`,
+ 1 AS `hora_pico`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `ruta`
+--
+
+DROP TABLE IF EXISTS `ruta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ruta` (
+  `id` int(11) DEFAULT NULL,
+  `num_buses` int(11) DEFAULT NULL,
+  `nombre_ruta` varchar(30) DEFAULT NULL,
+  KEY `RUTAS` (`id`,`num_buses`,`nombre_ruta`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ruta`
+--
+
+LOCK TABLES `ruta` WRITE;
+/*!40000 ALTER TABLE `ruta` DISABLE KEYS */;
+INSERT INTO `ruta` VALUES (NULL,1,'Villa Juárez Sarabia'),(NULL,2,'20 Aniversario (Directo)'),(NULL,2,'20 Aniversario (Inverso)'),(NULL,2,'Ruta 100'),(NULL,3,'Fco. Portillo (Directo)'),(NULL,3,'Infonavit (Directo)'),(NULL,4,'Av. Zarco (Esperanza)'),(NULL,4,'Av. Zarco (Martín López)'),(NULL,4,'Av. Zarco (Zootecnia)'),(NULL,4,'Circuito Universitario'),(NULL,4,'Panamericana (San Felipe)'),(NULL,4,'Villas del Norte (Directo)'),(NULL,4,'Villas del Norte (Inverso)'),(NULL,5,'Komatsu (Directo)'),(NULL,5,'Komatsu (Inverso)'),(NULL,5,'Panamericana (Mirador)'),(NULL,5,'Ruta 15 (Directo)'),(NULL,5,'Ruta 15 (inverso)'),(NULL,5,'Sierra Azul'),(NULL,6,'Camino Real'),(NULL,6,'Infonavit (Inverso)'),(NULL,6,'Jardines de Oriente'),(NULL,6,'Nombre de Dios Ojo (Directo)'),(NULL,6,'Nombre de Dios Ojo (Inverso)'),(NULL,6,'Ruta 3 (Directo)'),(NULL,6,'Ruta 3 (Inverso)'),(NULL,7,'Bolivar Zarco (104a)'),(NULL,7,'Dale Up'),(NULL,7,'Fco. Portillo (Inverso)'),(NULL,8,'Dos de Octubre'),(NULL,8,'Villas del Prado (Urbi)'),(NULL,9,'Campesina (Santa Rosa)'),(NULL,9,'Campesina (Sector 3)'),(NULL,9,'Cerro de la Cruz (11a)'),(NULL,9,'Cerro de la Cruz (Centro)'),(NULL,10,'Ávalos Plan de Ayala'),(NULL,10,'Tarahumara (Directo)'),(NULL,10,'Tarahumara (Inverso)'),(NULL,10,'Tec II (Colón)'),(NULL,10,'Tec II (Industrial)'),(NULL,11,'Villa Juárez Kennedy'),(NULL,14,'Punta Oriente'),(NULL,14,'Ramiro Valles Concordia'),(NULL,14,'Reloj Desarrollo Urbano'),(NULL,14,'Rosario');
+/*!40000 ALTER TABLE `ruta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `info_ruta` BEFORE UPDATE ON `ruta` FOR EACH ROW begin
+set new.nombre_ruta = 'La ruta es: ' + new.nombre_ruta;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Temporary view structure for view `rutas_abc`
+--
+
+DROP TABLE IF EXISTS `rutas_abc`;
+/*!50001 DROP VIEW IF EXISTS `rutas_abc`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `rutas_abc` AS SELECT 
+ 1 AS `id`,
+ 1 AS `num_buses`,
+ 1 AS `nombre_ruta`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `tiempo`
+--
+
+DROP TABLE IF EXISTS `tiempo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tiempo` (
+  `id` int(11) DEFAULT NULL,
+  `traslado` time DEFAULT NULL,
+  `retraso` time DEFAULT NULL,
+  `recorrido_total` time DEFAULT NULL,
+  KEY `TIEMPOR` (`id`,`traslado`,`retraso`,`recorrido_total`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tiempo`
+--
+
+LOCK TABLES `tiempo` WRITE;
+/*!40000 ALTER TABLE `tiempo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tiempo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `nuevo_retraso` BEFORE INSERT ON `tiempo` FOR EACH ROW begin
+set new.retraso = 'Nuevo retraso registrado';
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `traslado_total` BEFORE INSERT ON `tiempo` FOR EACH ROW begin
+set new.traslado = 'Tu tiempo de traslado es: ' + new.traslado;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `recorrido_final` BEFORE INSERT ON `tiempo` FOR EACH ROW begin
+set new.recorrido_total = 'Su tiempo de traslado total es: ' + (new.traslado - new.retraso); 
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping routines for database 'procedimientoalmacenado'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `mas_3_bus` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `mas_3_bus`(in num_buses int)
+begin
+select nombre_ruta, num_buses from ruta where num_buses > 3;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `retraso_0_20` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `retraso_0_20`(in retraso time)
+select * from tiempo where retraso < '00:20:00' ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `rutas_a_j` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `rutas_a_j`(in nombre_ruta varchar(30))
+begin 
+select * from ruta where nombre_ruta like '%a%' or nombre_ruta like '%j%';
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `retraso_limite`
+--
+
+/*!50001 DROP VIEW IF EXISTS `retraso_limite`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = cp850 */;
+/*!50001 SET character_set_results     = cp850 */;
+/*!50001 SET collation_connection      = cp850_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `retraso_limite` AS select `tiempo`.`id` AS `id`,`tiempo`.`traslado` AS `traslado`,`tiempo`.`retraso` AS `retraso`,`tiempo`.`recorrido_total` AS `recorrido_total` from `tiempo` where (`tiempo`.`retraso` like '01:00:00') */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `retraso_lluvia`
+--
+
+/*!50001 DROP VIEW IF EXISTS `retraso_lluvia`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = cp850 */;
+/*!50001 SET character_set_results     = cp850 */;
+/*!50001 SET collation_connection      = cp850_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `retraso_lluvia` AS select `factores`.`id` AS `id`,`factores`.`climatologico` AS `climatologico`,`factores`.`trafico` AS `trafico`,`factores`.`hora_pico` AS `hora_pico` from `factores` where (`factores`.`climatologico` like 'lluvia') */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `rutas_abc`
+--
+
+/*!50001 DROP VIEW IF EXISTS `rutas_abc`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = cp850 */;
+/*!50001 SET character_set_results     = cp850 */;
+/*!50001 SET collation_connection      = cp850_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `rutas_abc` AS select `ruta`.`id` AS `id`,`ruta`.`num_buses` AS `num_buses`,`ruta`.`nombre_ruta` AS `nombre_ruta` from `ruta` where ((`ruta`.`nombre_ruta` like 'a%') or (0 <> 'b%') or (0 <> 'c%')) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-21  0:05:52
